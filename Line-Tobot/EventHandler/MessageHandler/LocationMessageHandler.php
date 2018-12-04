bb669f4b79e90ea2a9804b6e2a0e356e1df2de27
Tomohide Higashinaka <higxtom@gmail.com>
2018-12-04 11:15:38 +0900

    get station data

--- Line-Tobot/EventHandler/MessageHandler/LocationMessageHandler.php
+++ Line-Tobot/EventHandler/MessageHandler/LocationMessageHandler.php
@@ -41,11 +41,11 @@ class LocationMessageHandler implements EventHandler
         //error_log($candidates);
         // 500 -> 1000 -> 1500
         $stations = json_decode(getNearestStations($latitude, $longitude, $candidates, 500), true);
-        error_log(print_r($stations,true));
+//         error_log(print_r($stations,true));
         $rmsg = "Near stations where you are.";
         foreach ($stations as $station) {
             error_log(print_r($station, true));
-//             $rmsg .= $station['station_name'] . "(" . $station['line_name'] . ")" . ":" . number_format($station['dist'],0) . "m";
+            $rmsg .= $station[0] . "(" . $station[1] . ")" . ":" . number_format($station[2],0) . "m";
         }
         
         $rmsg .= "You are at " . $owm_data['name'] . ", and the weather focast is " . $owm_data['weather'][0]['main'] . '(' . $owm_data['weather'][0]['description'] . ')';
